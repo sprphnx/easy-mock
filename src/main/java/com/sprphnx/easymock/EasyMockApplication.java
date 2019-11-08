e4081858ab783505fe641c3dd5a757a4d959cc73
sprphnx <sprphnx@outlook.com>
2019-11-08 09:26:50 +0300

    Initial commit
    Created spring-boot project with Web and Lombok dependency.

---
+++ src/main/java/com/sprphnx/easymock/EasyMockApplication.java
@@ -0,0 +1,13 @@
+package com.sprphnx.easymock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyMockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sprphnx/easymock/EasyMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprphnx.easymock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
